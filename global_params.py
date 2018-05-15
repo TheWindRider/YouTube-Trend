eb195a442e84ef6d30df65e11e21cf77fdf1fe15
Chenguang Yang <ychg235711@gmail.com>
2018-05-14 21:06:27 -0700

    More updates using global params

--- global_params.py
+++ global_params.py
@@ -9,17 +9,19 @@ class Params:
     FILE_NAME_VOCAB = "vocabulary.tsv"
     FILE_NAME_MODEL = "title_category.npz"
     # text data
-    MAX_DOC_LENGTH = 10
+    MAX_DOC_LENGTH = 15
     PADWORD = 'ZYXW'
     NUM_CLASS = 16
     # neural network
     EMBED_SIZE = 16
     CONV_SIZE = 5
     CONV_STRIDE = 2
-    CONV_FILTER = 32
-    HIDDEN_FILTER = 256
+    CONV_FILTER = 4
+    HIDDEN_FILTER = 64
     # training
+    SPLIT_RATE = 0.2
     DROP_RATE = 0.3
-    LEARN_RATE = 0.001
-    BATCH_SIZE = 128
+    LEARN_RATE = 0.0005
+    BATCH_SIZE = 64
+    NUM_EPOCH = 50
     NUM_STEP = 1000
